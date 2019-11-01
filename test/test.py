0610006ed5f2a9176fff38fbdebe7856dbf94159
Quentin Comte-Gaz <quentin@comte-gaz.com>
2019-11-01 21:45:50 +0100

    test: Add real mock to unit-test (#20)

--- test/test.py
+++ test/test.py
@@ -8,12 +8,70 @@
 
 import unittest
 from GSMTC35 import GSMTC35
+from unittest.mock import patch
 
-class AutomaticTestGSMTC35(unittest.TestCase):
-  def test_fail_setup(self):
-    gsm = GSMTC35.GSMTC35()
-    self.assertFalse(gsm.setup(_port="COM_Invalid", _pin="1234", _puk="12345678", _pin2="4321", _puk2="87654321"))
+class MockSerial:
+  """
+  TODO: Explanation of the class + functions
+  """
+  __is_open = True
+  __read_write = []
 
+  default_read_write_for_setup = [
+      {'IN': b'AT+IPR=0\r\n'}, {'OUT': b'OK\r\n'},
+      {'IN': b'ATE0\r\n'}, {'OUT': b'OK\r\n'},
+      {'IN': b'ATV1\r\n'}, {'OUT': b'OK\r\n'},
+      {'IN': b'AT+CMEE=0\r\n'}, {'OUT': b'OK\r\n'},
+      {'IN': b'AT+CPIN?\r\n'}, {'OUT': b'+CPIN: READY\r\n'}, {'OUT': b'OK\r\n'},
+      {'IN': b'AT+CLIP=0\r\n'}, {'OUT': b'OK\r\n'},
+      {'IN': b'AT+CNMI=0,0\r\n'}, {'OUT': b'OK\r\n'},
+      {'IN': b'AT^SCTM=0\r\n'}, {'OUT': b'OK\r\n'},
+      {'IN': b'AT+CMGF=1\r\n'}, {'OUT': b'OK\r\n'},
+      {'IN': b'AT+IPR=115200\r\n'}, {'OUT': b'OK\r\n'}
+    ]
+
+  @staticmethod
+  def initializeMock(read_write, is_open = True):
+    MockSerial.__is_open = is_open
+    MockSerial.__read_write = read_write
+
+  def __init__(self, port="", baudrate="", parity="", stopbits="", bytesize="", timeout=""):
+    return
+
+  def inWaiting(self):
+    if MockSerial.__is_open and len(MockSerial.__read_write) > 0:
+      if 'OUT' in MockSerial.__read_write[0]:
+        return len(MockSerial.__read_write[0]['OUT'])
+    return 0
+
+  def read(self, dummy):
+    if MockSerial.__is_open and len(MockSerial.__read_write) > 0:
+      if 'OUT' in MockSerial.__read_write[0]:
+        val = MockSerial.__read_write[0]['OUT']
+        MockSerial.__read_write.pop(0)
+        return val
+    return ""
+
+  def write(self, data):
+    if MockSerial.__is_open and len(MockSerial.__read_write) > 0:
+      if 'IN' in MockSerial.__read_write[0]:
+        check_val = MockSerial.__read_write[0]['IN']
+        if str(data) != str(check_val):
+          raise AssertionError('Mock Serial: Should write "' + str(check_val) + '" but "'+str(data)+'" requested')
+        MockSerial.__read_write.pop(0)
+        return len(data)
+    return 0
+
+  def isOpen(self):
+    return MockSerial.__is_open
+
+  def close(self):
+    return True
+
+class TestGSMTC35(unittest.TestCase):
+  """
+  TODO: Explanation of the class + functions
+  """
   def test_cmd_fail(self):
     # Request failed because nothing requested
     with self.assertRaises(SystemExit) as cm:
@@ -99,6 +157,17 @@ class AutomaticTestGSMTC35(unittest.TestCase):
     with self.assertRaises(SystemExit) as cm:
       GSMTC35.main((["--help", "information"]))
     self.assertEqual(cm.exception.code, 0)
+
+  def test_fail_setup(self):
+    gsm = GSMTC35.GSMTC35()
+    self.assertFalse(gsm.setup(_port="COM_Invalid", _pin="1234", _puk="12345678", _pin2="4321", _puk2="87654321"))
+
+  @patch('serial.Serial', new=MockSerial)
+  def test_success_setup(self):
+    MockSerial.initializeMock(MockSerial.default_read_write_for_setup)
+
+    gsm = GSMTC35.GSMTC35()
+    self.assertTrue(gsm.setup(_port="COM_FAKE", _pin="1234", _puk="12345678"))
 
 if __name__ == '__main__':
   unittest.main()
